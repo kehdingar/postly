1e43a728953841be408a79bd5ab96e0d2d9a0dd3
Kehdinga Raymond <kehdingar@gmail.com>
2023-10-09 22:20:21 +0100

    test-feat: add setup for tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -7,16 +7,27 @@ from rest_framework import status
 User = get_user_model()
 
 class UserTests(APITestCase):
+    def setUp(self):
+        # Create a user for testing
+        self.user = User.objects.create_user(
+            username='testuser',
+            email='testuser@example.com',
+            password='testpass123',
+            first_name='Test',
+            last_name='User'
+        )    
+
     def test_create_user(self):
         url = reverse('register')
         data = {
-            'username': 'testuser',
-            'email': 'testuser@example.com',
-            'password': 'testpass123',
-            'first_name': 'Test',
-            'last_name': 'User'
+            'username': 'testuser2',
+            'email': 'testuser2@example.com',
+            'password': 'test2pass123',
+            'first_name': 'Test2',
+            'last_name': 'User2'
         }
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(User.objects.count(), 1)
-        self.assertEqual(User.objects.get().username, 'testuser')
+        self.assertEqual(User.objects.count(), 2)
+        self.assertEqual(User.objects.get(username='testuser2').username, 'testuser2')
+
