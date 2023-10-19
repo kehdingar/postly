0e050ba03c9f8ef08f69cd12fe3a1c9348e9cbd5
Kehdinga Raymond <kehdingar@gmail.com>
2023-10-19 18:01:12 +0100

    test: add test for get post detail

--- posts/tests.py
+++ posts/tests.py
@@ -29,3 +29,10 @@ class PostTests(TestCase):
         serializer = PostSerializer(posts, many=True)
         self.assertEqual(response.data, serializer.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_post_detail(self):
+        url = f'/posts/{self.post.id}/'
+        response = self.client.get(url)
+        serializer = PostSerializer(self.post)
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)        
