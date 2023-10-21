24506dd1a3c431f03a23140d0958f404051b0834
Kehdinga Raymond <kehdingar@gmail.com>
2023-10-21 15:22:50 +0100

    feat: add like for posts

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework import routers
-from .views import PostViewSet
+from .views import PostViewSet, LikeAPIView
 
 router = routers.DefaultRouter()
 router.register(r'', PostViewSet)
@@ -8,6 +8,7 @@ router.register(r'', PostViewSet)
 # This creates the standard URL patterns for the viewset, such as /posts/ and /posts/<int:pk>/
 
 urlpatterns = [
+    path('<int:pk>/likes/', LikeAPIView.as_view(), name='like'),
     path('<int:pk>/like/', PostViewSet.as_view({'post': 'like'}), name='like'),    
 ]
 
--- posts/views.py
+++ posts/views.py
@@ -49,3 +49,26 @@ class PostViewSet(viewsets.ModelViewSet):
                 return Response({'error': 'You can only update posts you create'}, status=status.HTTP_403_FORBIDDEN)
         except Exception as e:
             return Response({'error': 'Post does not exists'}, status=status.HTTP_404_NOT_FOUND)
+        
+    # This like action is placed here for convinience and consistency sice like is associated with posts
+    @action(detail=True, methods=['post'])
+    def like(self, request, pk=None):
+        post = self.get_object()
+        user = request.user
+        like = Like.objects.filter(post=post, user=user).first()
+        if like:
+            like.delete()
+            return Response({'detail': 'Post unliked.'}, status=status.HTTP_200_OK)
+        else:
+            Like.objects.create(post=post, user=user)
+            return Response({'detail': 'Post liked.'}, status=status.HTTP_201_CREATED)
+
+
+class LikeAPIView(generics.ListAPIView):
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        post_id = self.kwargs['pk']
+        return Like.objects.filter(post=post_id)
