3dfc934c55fc18ccdb4ac9d7da6cce59f7f89b8a
Kehdinga Raymond <kehdingar@gmail.com>
2023-10-27 12:36:34 +0100

    config: add admin config for posts

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Post, Like
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'created_at', 'updated_at','score']
+    list_filter = ['created_at', 'updated_at']
+    search_fields = ['title', 'content', 'author__username']
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ['post', 'user', 'created_at']
+    list_filter = ['created_at']
+    search_fields = ['post__title', 'user__username']
+
